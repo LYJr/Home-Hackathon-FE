b67936ae6e5ba611d5343b04b5e4daaee631a74e
LYJr <lyi91@naver.com>
2023-06-09 17:23:43 +0900

    버그 수정 onClick -> onChange

--- src/commponent/Tables/TableControllro.js
+++ src/commponent/Tables/TableControllro.js
@@ -1,3 +1,21 @@
+function CreateTableHeaderRow(col, setCol, index) {
+    let copyCol = [...col];
+
+    if(index === 0) {
+        return
+    }
+
+    return (
+        <input type = "text" value={copyCol[index]} 
+            name = {"thead"+0+index}
+            onChange={(e) => {
+                copyCol[index] = e.target.value;
+                setCol(copyCol);
+            }}>
+        </input> 
+    );
+}
+
 function CreateTableBodyRow(row, setRow, col, rowIndex) {
     let copyCol = [...col];
     let copyRow = [...row];
@@ -9,22 +27,18 @@ function CreateTableBodyRow(row, setRow, col, rowIndex) {
                 <td>
                     <input type ="checkbox"  
                         name={"checkbox"+rowIndex}
-                        onClick={(e) => {
-                            //체크여부를 확인하고 싶음.
-                            console.log("뭐 들어오냐 :"+e.getModifierState)
-                            copyRow[rowIndex][0] = true;
-                            setRow(copyRow)
-                        }}>
+                        >
                     </input> 
                 </td>;
             continue;
         }
+
         rows[i] = 
             <td>
                 <input type = "text" 
                     value={copyRow[rowIndex][i]} 
                     name={"tbody"+(rowIndex+1)+i}
-                    onClick={(e) => {
+                    onChange={(e) => {
                         copyRow[rowIndex][i] = e.target.value;
                         setRow(copyRow);
                     }}>
@@ -34,21 +48,6 @@ function CreateTableBodyRow(row, setRow, col, rowIndex) {
     return rows
 }
 
-function CreateTableHeaderRow(col, setCol, index) {
-    let copyCol = [...col];
 
-    if(index === 0) {
-        return
-    }
-
-    return (
-            <input type = "text" value={copyCol[index]} name = {"thead"+0+index}
-                onChange={(e) => {
-                    copyCol[index] = e.target.value;
-                    setCol(copyCol);
-                }}>
-            </input> 
-    );
-}
 
 export {CreateTableBodyRow, CreateTableHeaderRow};
